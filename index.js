8afcf100a089e22c6b1e462b8e1b5ee34822a30a
Guin <65201257+guianginamo19@users.noreply.github.com>
2020-09-22 20:49:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Discord = require("discord.js")
-const config = require(./config.json")
+const config = require("./config.json")
 const client = new Discord.Client({disableEveryone: true})
 
+client.on('message', async message {
+let prefix = "+"
+if(message.content.startsWith(`${prefix}ping`){
+return message.channel.send("Pong")
+}
 bot.login(config.token)
