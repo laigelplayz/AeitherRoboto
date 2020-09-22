62010d72a0068681ee0909b91ce1ea69aaa68e92
Guin <65201257+guianginamo19@users.noreply.github.com>
2020-09-22 21:13:22 +0800

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+
+return message.channel.send({embed: {
+color: 15158332,
+title: "Pong",
+description: `${Date.now() - message.createdTimestamp}\`ms`,
+footer: {
+text: client.user.username + "Quack"
+}
+}
+})
+}
+
+module.exports.config = {
+name: "ping",
+aliases: []
+}
